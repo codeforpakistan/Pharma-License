1b32f84fc280b27b8d9836e37be2e56c76fcee98
awais <muhammadawais@codeforpakistan.org>
2020-12-11 20:01:17 +0500

    changes

--- application/config/config.php
+++ application/config/config.php
@@ -327,7 +327,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
  */
-$config['encryption_key'] = 'kp_pharma';
+$config['encryption_key'] = '';
 
 /*
 |--------------------------------------------------------------------------
