f5622c70506945c6602fddc0b7e6b9c5bcdc4a5d
awais <muhammadawais@codeforpakistan.org>
2021-01-18 18:57:28 +0500

    add footer logos

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,19 +1,22 @@
+<!-- Main Footer -->
+<footer class="main-footer no-print">
+  <!-- To the right -->
+  <div class="pull-right hidden-xs">
+    <img src="https://kpitb.gov.pk/sites/default/files/kpitb-logo_0.png" width="120px;" class="user-image" />
+    <img src="https://www.durshal.com/sites/default/files/Durshal-GIL-Black_0.png" width="120px;" class="user-image" />
+    <img src="https://codeforpakistan.org/wp-content/uploads/2014/11/Code_for_Pakistan_003_OL-011-e1416691355388.png" width="120px;" class="user-image" />
+    <img src="https://logos-download.com/wp-content/uploads/2016/03/The_World_Bank_Group_logo.png" width="120px;" class="user-image" />
 
-  <!-- Main Footer -->
-  <footer class="main-footer no-print">
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
-      By Drug Control & Pharmacy Services Health Department KP
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; <?php echo date('Y') ?> <a href="#">Drug Control & Pharmacy Services Health Department KP</a>.</strong> All rights reserved.
-  </footer>
+  </div>
+  <!-- Default to the left -->
+  <strong>Copyright &copy; <?php echo date('Y') ?><br> <a href="#">Drug Control & Pharmacy Services Health Department KP</a>.</strong> All rights reserved.<br>
+</footer>
 
 
-  <!-- Add the sidebar's background. This div must be placed
+<!-- Add the sidebar's background. This div must be placed
        immediately after the control sidebar -->
-  <div class="control-sidebar-bg"></div>
+<div class="control-sidebar-bg"></div>
 <!-- </div> -->
 <!-- ./wrapper -->
 
-  <?php $this->load->view('templates/endhtml');?>
+<?php $this->load->view('templates/endhtml'); ?>
